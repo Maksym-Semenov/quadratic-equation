1ea7c644b56883d7fbfeb0f15f1b97b2ba2de5db
Maksym-Semenov <mestcomua@gmail.com>
2022-06-14 22:56:17 +0300

    update_1

--- Program.cs
+++ Program.cs
@@ -1,58 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Квадратное_уравнение
+namespace quadratic_equation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            while(true)
+            while (true)
             {
                 Console.Clear();
-
                 Console.WriteLine("Эта программа решает квадратные уравнения вида ax2+bx+c=0");
                 Console.Write("Введите первый коэффициент A ");
-                double a = int.Parse(Console.ReadLine());
+                bool successA = int.TryParse(Console.ReadLine(), out int a);
                 Console.Write("Введите второй коэффициент B ");
-                double b = int.Parse(Console.ReadLine());
+                bool successB = int.TryParse(Console.ReadLine(), out int b);
                 Console.Write("Введите третий коэффициент C ");
-                double c = int.Parse(Console.ReadLine());
+                bool successC = int.TryParse(Console.ReadLine(), out int c);
 
+                Diskriminant diskriminant = new Diskriminant(a, b, c);
+                double result = diskriminant.GetDiskriminant(a, b, c);
 
-                double diskriminant = Math.Pow(b, 2) - (4 * a * c);
-                double Sqrt = Math.Sqrt(diskriminant);
-
-                double x1 = (((-1) * b) + Sqrt) / (2 * a);
-
-                double x2 = (((-1) * b) - Sqrt) / (2 * a);
-
-                double x12 = (b / (2 * a) * (-1));
-
-                if (diskriminant < 0)
-                {
-                    Console.WriteLine("Дискриминант меньше 0, поэтому уравнение не имеет рациональных корней");
-                }
-                else if (diskriminant == 0)
-                {
-                    Console.Write("Дискриминант равен 0, решение уравнения имеет два равных корня x = " + x12);
-                }
-                else if (diskriminant > 0)
-                {
-                    Console.WriteLine("Дискриминант больше 0, поэтому есть два решения ");
-                    Console.WriteLine("Первый корень уравнения x1 = " + x1);
-                    Console.WriteLine("Второй корень уравнения x2 = " + x2);
-                }
-
+                Respons respons = new Respons();
+                Console.WriteLine(respons);
                 Console.ReadLine();
-                Random rnd = new Random();  
+
+                //if (result < 0)
+                //{
+                //    Console.WriteLine("Дискриминант меньше 0, поэтому уравнение не имеет рациональных корней");
+                //}
+                //else if (result == 0)
+                //{
+                //    Console.Write("Дискриминант равен 0, решение уравнения имеет два равных корня x = " + x12);
+                //}
+                //else if (result > 0)
+                //{
+                //    Console.WriteLine("Дискриминант больше 0, поэтому есть два решения ");
+                //    Console.WriteLine("Первый корень уравнения x1 = " + x1);
+                //    Console.WriteLine("Второй корень уравнения x2 = " + x2);
+                //}
+                //Console.ReadLine();
             }
-            
-
-
         }
     }
 }
---
+++ Respons.cs
@@ -0,0 +1,16 @@
+﻿namespace quadratic_equation
+{
+    public class Respons
+    {
+        public double X1;
+        public double X2;
+        public double X12;
+        public string GetRespons(double diskriminant, int A, int B)
+            {
+            double X1 = (((-1) * B) + diskriminant) / (2 * A);
+            double X2 = (((-1) * B) - diskriminant) / (2 * A);
+            double X12 = (B / (2 * A) * (-1));
+            return X1.ToString() + X2.ToString() + X12.ToString();
+        }
+    }
+}
