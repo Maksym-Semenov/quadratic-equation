e2108e3fac950bd65de726daf940e3c4db95ec16
Maksym-Semenov <mestcomua@gmail.com>
2022-06-17 21:09:41 +0300

    update

--- Program.cs
+++ Program.cs
@@ -11,12 +11,29 @@ namespace quadratic_equation
                 Console.Clear();
                 Console.WriteLine("Эта программа решает квадратные уравнения вида ax2+bx+c=0");
                 Console.Write("Введите первый коэффициент A ");
-                _ = int.TryParse(Console.ReadLine(), out int a);
+                if(int.TryParse(Console.ReadLine(), out int a) && a!= 0)
+                    Console.ReadLine();
+                else
+                {
+                    Console.WriteLine("Введите не 0");
+                    continue;
+                }
                 Console.Write("Введите второй коэффициент B ");
-                _ = int.TryParse(Console.ReadLine(), out int b);
+                if(int.TryParse(Console.ReadLine(), out int b) && b != 0)
+                    Console.ReadLine();
+                else
+                {
+                    Console.WriteLine("Введите не 0");
+                    continue;
+                }
                 Console.Write("Введите третий коэффициент C ");
-                _ = int.TryParse(Console.ReadLine(), out int c);
-
+                if(int.TryParse(Console.ReadLine(), out int c) && c != 0)
+                    Console.ReadLine();
+                else
+                {
+                    Console.WriteLine("Введите не 0");
+                    continue;
+                }
                 Diskriminant diskriminant = new Diskriminant();
                 double result = diskriminant.GetDiskriminant(a, b, c);
 
